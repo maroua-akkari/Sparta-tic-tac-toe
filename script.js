cc93a0bc4a2a5fa7e8bc996ee006a1a8a3d34240
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2019-04-12 00:56:59 +0100

    took out comments

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ $(function(event) {
 
 
   var player1 = prompt("Player 1, take your pick: 'O' or 'X'");
-  // var player2 = prompt("Player 2, take your pick: 'O' or 'X'");
 
   if (player1 == "X") {
     $("td").on("click", function() {
@@ -25,62 +24,6 @@ $(function(event) {
     console.log("reset");
     player1;
   })
-
-
-
-//
-// $("td").on("click", function(){
-//   var boxClicked = $(this).data("position");
-//   console.log(boxClicked);
-// })
-
-//
-// var boxes = $("td");
-// for (var i = 0; i < boxes.length; i++) {
-//   var box = boxes[i];
-//
-// console.log(boxes);
-// console.log(box);
-// }
-
-
-
-
-
-// $("td").on("click", function() {
-//   $(box).each(function(index, box) {
-//     var box = boxes[i];
-//     $(this).html("X").css("color", "white").addClass("X");
-// })
-// })
-
-
-  // $("td").on("click", function() {
-  //   console.log("this box was clicked");
-  //   $(this).html("X").css("color", "white").addClass("X");
-  // })
-
-
-  // $("td").on("click", function() {
-  //   if (player == 1) {
-  //     $(this).html("X").css("color", "white").addClass("X");
-  //   } else if (player ==2) {
-  //     $(this).html("X").css("color", "black").addClass("O");
-  //   }   console.log("this box was clicked");;
-  // })
-
-
-   $("tr").addClass("column");
-   $("td").addClass("row");
-
-   // $(".row").on("click", function(){
-   //   $(this).css("background", "white").html("O");
-   // })
-
-// $("table").on("click", function() {
-// $(this).rows[2].cells;
-//   x[0].innerHTML = "hello";
-// })
 
 
 
